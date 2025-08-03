37358242bb2dcd38a184d114c3e70a82ecd433fd
Maneesha Yasmith <106625118+maneeyash@users.noreply.github.com>
2025-08-04 00:56:37 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,16 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1:2c75a01f-e4c6-4325-a7ef-53c6474e58a5
-', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'd1b7llathgbkw4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_uIcrP796E'
+
+, // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2r40149640i5b8p3lpnafhnlvk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
